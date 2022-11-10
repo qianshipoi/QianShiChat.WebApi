72b86415b187ae7bbdbb6c3b0f3fab840e2d4c64
未来 栗山 <2410128587@qq.com>
2022-11-10 21:01:47 +0800

    message text.

--- src/QianShiChat.WebApi/Hubs/ChatHub.cs
+++ src/QianShiChat.WebApi/Hubs/ChatHub.cs
@@ -15,4 +15,7 @@ public class ChatHub : Hub
     {
         return base.OnDisconnectedAsync(exception);
     }
+
+    public async Task SendMessage(string user, string message)
+      => await Clients.All.SendAsync("ReceiveMessage", user, message);
 }
