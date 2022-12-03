0c5fb122b9700546dc6ca834bdd94eb8167c9f30
未来 栗山 <2410128587@qq.com>
2022-12-03 21:01:44 +0800

    add imagesharp component.

--- src/QianShiChat.WebApi/Program.cs
+++ src/QianShiChat.WebApi/Program.cs
@@ -1,4 +1,11 @@
 // config distributed id.
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Web.Caching;
+using SixLabors.ImageSharp.Web.Commands;
+using SixLabors.ImageSharp.Web.DependencyInjection;
+using SixLabors.ImageSharp.Web.Processors;
+using SixLabors.ImageSharp.Web.Providers;
+
 var options = new IdGeneratorOptions(1);
 YitIdHelper.SetIdGenerator(options);
 
@@ -37,6 +44,37 @@ builder.Services
     .AddSaveChatMessageJob();
 
 builder.Services.AddDirectoryBrowser();
+builder.Services.AddImageSharp(options =>
+{
+    options.Configuration = Configuration.Default;
+    options.BrowserMaxAge = TimeSpan.FromDays(7);
+    options.CacheMaxAge = TimeSpan.FromDays(365);
+    options.CacheHashLength = 8;
+    options.OnParseCommandsAsync = _ => Task.CompletedTask;
+    options.OnBeforeSaveAsync = _ => Task.CompletedTask;
+    options.OnProcessedAsync = _ => Task.CompletedTask;
+    options.OnPrepareResponseAsync = _ => Task.CompletedTask;
+})
+                .SetRequestParser<QueryCollectionRequestParser>()
+                .Configure<PhysicalFileSystemCacheOptions>(options =>
+                {
+                    options.CacheRootPath = null;
+                    options.CacheFolder = "is-cache";
+                    options.CacheFolderDepth = 8;
+                })
+                .SetCache<PhysicalFileSystemCache>()
+                .SetCacheKey<UriRelativeLowerInvariantCacheKey>()
+                .SetCacheHash<SHA256CacheHash>()
+                .Configure<PhysicalFileSystemProviderOptions>(options =>
+                {
+                    options.ProviderRootPath = null;
+                })
+                .AddProvider<PhysicalFileSystemProvider>()
+                .AddProcessor<ResizeWebProcessor>()
+                .AddProcessor<FormatWebProcessor>()
+                .AddProcessor<BackgroundColorWebProcessor>()
+                .AddProcessor<QualityWebProcessor>()
+                .AddProcessor<AutoOrientWebProcessor>();
 
 var app = builder.Build();
 
@@ -45,6 +83,7 @@ app.UseOpenApi();
 app.UseCors(AppConsts.MyAllowSpecificOrigins);
 
 app.UseDefaultFiles();
+app.UseImageSharp();
 app.UseStaticFiles();
 app.UseDirectoryBrowser(new DirectoryBrowserOptions
 {
