abf249a302dd37b0c36f59cd10ae4856125dba0e
未来 栗山 <2410128587@qq.com>
2022-12-20 18:05:27 +0800

    remove using.

--- src/QianShiChat.WebApi/Migrations/ChatDbContextModelSnapshot.cs
+++ src/QianShiChat.WebApi/Migrations/ChatDbContextModelSnapshot.cs
@@ -2,7 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using QianShiChat.WebApi.Models;
+
 
 #nullable disable
 
