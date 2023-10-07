df21928604a5dfe4577866a959099eef4d35ccb3
未来 栗山 <2410128587@qq.com>
2023-10-07 22:08:36 +0800

    fix: migration database

---
+++ src/QianShiChat.Infrastructure/Data/Migrations/20231007114622_v0.0.4.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QianShiChat.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class v004 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Alias",
+                table: "UserRealtion",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Alias",
+                table: "UserGroupRealtion",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "UserRealtion",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Alias",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "UserRealtion",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Alias",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "UserRealtion",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Alias",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "UserRealtion",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Alias",
+                value: null);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Alias",
+                table: "UserRealtion");
+
+            migrationBuilder.DropColumn(
+                name: "Alias",
+                table: "UserGroupRealtion");
+        }
+    }
+}
