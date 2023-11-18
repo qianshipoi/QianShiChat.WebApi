b6fa0e234a12336e4437b6ab9c1a03e2dc73ef4c
未来 栗山 <2410128587@qq.com>
2023-11-18 22:57:26 +0800

    feat: group dto add users

--- src/QianShiChat.Application.Contracts/GroupDto.cs
+++ src/QianShiChat.Application.Contracts/GroupDto.cs
@@ -8,4 +8,5 @@ public class GroupDto : IRoomToObject
     public int TotalUser { get; set; }
     public long CreateTime { get; set; }
     public string Avatar { get; set; } = string.Empty;
+    public List<UserDto> Users { get; set; } = new List<UserDto>();
 }
